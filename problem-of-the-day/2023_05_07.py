c32a18faa31527b3fa16544efaaab50a8e69fd8d
Suhrid Mathur <mathursuhrid@gmail.com>
2023-05-07 00:21:27 +0530

    Add POTD 6th May 2023

---
+++ problem-of-the-day/2023_05_07.py
@@ -0,0 +1,17 @@
+
+# 1498. Number of Subsequences That Satisfy the Given Sum Condition
+# https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/description/
+
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        l, r = 0, len(nums) - 1
+        res = 0
+        mod = 10**9 + 7
+        while l <= r:
+            if nums[l] + nums[r] > target:
+                r -= 1
+            else:
+                res += pow(2, r - l, mod)
+                l += 1
+        return res % mod
